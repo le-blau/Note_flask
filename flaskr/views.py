6654e7ba3734f910867a96413228352021dc574d
blau <>
2020-03-02 02:08:04 +0900

    feat: ログイン機能

--- flaskr/views.py
+++ flaskr/views.py
@@ -1,6 +1,6 @@
-from flask import request, redirect, url_for, render_template, flash
+from flask import request, redirect, url_for, render_template, flash, abort, jsonify, session
 from flaskr import app, db
-from flaskr.models import Entry
+from flaskr.models import Entry, User
 
 @app.route('/')
 def show_entries():
@@ -35,4 +35,77 @@ def delete_one_entry(entry):
     db.session.delete(post)
     db.session.commit()
     flash('Deleted one entry')
+    return redirect(url_for('show_entries'))
+
+
+# ユーザー用view
+# ユーザー一覧
+@app.route('/users/')
+def user_list():
+    users = User.query.all()
+    return render_template('user/list.html', users=users)
+
+# ユーザー詳細
+@app.route('/users/<int:user_id>/')
+def user_detail(user_id):
+    user = User.query.get(user_id)
+    return render_template('user/detail.html', user=user)
+
+# ユーザー編集
+@app.route('/users/<int:user_id>/edit/', methods=['GET', 'POST'])
+def user_edit(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        abort(404)
+    if request.method == 'POST':
+        user.name=request.form['name']
+        user.email=request.form['email']
+        user.password=request.form['password']
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user_detail', user_id=user_id))
+    return render_template('user/edit.html', user=user)
+
+# ユーザーの作成
+@app.route('/users/create/', methods=['GET', 'POST'])
+def user_create():
+    if request.method == 'POST':
+        user = User(name=request.form['name'],
+                    email=request.form['email'],
+                    password=request.form['password'])
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('user_list'))
+    return render_template('user/edit.html')
+
+# ユーザー削除
+@app.route('/users/<int:user_id>/delete/', methods=['DELETE'])
+def user_delete(user_id):
+    user = User.query.get(user_id)
+    if user is None:
+        response = jsonify({'status': 'Not Found'})
+        response.status_code = 404
+        return response
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'status': 'OK'})
+
+
+# ログイン画面
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user, authenticated = User.authenticate(db.session.query, request.form['email'], request.form['password'])
+        if authenticated:
+            session['user_id'] = user.id
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+        else:
+            flash('Invalid email or password')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    flash('You were logged out')
     return redirect(url_for('show_entries'))
--- manage.py
+++ manage.py
@@ -1,3 +1,15 @@
 #flaskrを起動して実行するためのファイル
-from flaskr import app
-app.run(host='127.0.0.1', port=5000, debug=True)
+from __future__ import print_function
+from flask_script import Manager
+from flaskr import app, db
+
+manager = Manager(app)
+
+@manager.command
+def init_db():
+    db.create_all()
+
+if __name__ == '__main__':
+    manager.run()
+
+# app.run(host='127.0.0.1', port=5000, debug=True)
